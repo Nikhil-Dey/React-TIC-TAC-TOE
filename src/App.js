7a608a8e8d76afb9a776a6ef126155a356809311
Nikhil Dey <deynikhil963@gmail.com>
2024-09-28 15:23:00 +0530

    Added travelling History Feature

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ function Square({value, onSquareClick}){
   return <button className='square' onClick={onSquareClick}>{value}</button>
 }
 
-export default function Board(){
-  const[nextmove, setNextMove] = useState(true);
-  const[squares, setSquares] = useState(Array(9).fill(null));
+function Board({isNextMove, squares, onPlay}){
 
   function onHandleClick(i){
     if(squares[i] || CalculateWinner(squares)){
@@ -14,14 +12,13 @@ export default function Board(){
     }
 
     const newSqares = squares.slice();
-    if(nextmove){
+    if(isNextMove){
       newSqares[i] = 'X';
     }else{
       newSqares[i] = 'O';
     }
 
-    setNextMove(!nextmove);
-    setSquares(newSqares);
+    onPlay(newSqares);
   }
 
   let winner = CalculateWinner(squares);
@@ -29,7 +26,7 @@ export default function Board(){
   if (winner){
     status = 'Winner: ' + winner;
   }else{
-    status = 'Next Player: ' + ((nextmove)? 'X' : 'O');
+    status = 'Next Player: ' + ((isNextMove)? 'X' : 'O');
   }
 
   return (
@@ -52,6 +49,53 @@ export default function Board(){
       </div>
     </>
   );
+}
+
+export default function Game(){
+  // const[nextmove, setNextMove] = useState(true);
+  const[history, setHistory] = useState([Array(9).fill(null)]);
+  const[currentMove, setCurrentMove] = useState(0);
+  const nextmove = currentMove % 2 === 0;
+
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares){
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+    // setNextMove(!nextmove);
+  }
+
+  function jumpTo(move){
+    setCurrentMove(move);
+    // setNextMove(move % 2 === 0);
+  }
+
+  const moves = history.map((squares, index) => {
+    let description;
+    if(index > 0){
+      description = "Go to move #" + index;
+    }else{
+      description = "Go to game start";
+    }
+
+    return (
+      <li key={index}>
+      <button onClick={()=>jumpTo(index)}>{description}</button>
+    </li>
+    )
+  });
+
+  return (
+    <div className='game'>
+      <div className='game-board'>
+      <Board isNextMove={nextmove} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className='game-info'>
+        {moves}
+      </div>
+    </div>
+  )
 }
 
 function CalculateWinner(squares){
